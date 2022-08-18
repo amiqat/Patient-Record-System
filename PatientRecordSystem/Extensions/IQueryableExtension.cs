a74ad6e9a1a0c1253817c887972b60acd61ede36
Ameen Luay <ameen.alqattow@cepres.com>
2022-08-18 10:27:39 +0300

    upgrade

--- PatientRecordSystem/Extensions/IQueryableExtension.cs
+++ PatientRecordSystem/Extensions/IQueryableExtension.cs
@@ -8,18 +8,15 @@ namespace PatientRecordSystem.Extensions
 {
     public static class IQueryableExtension
     {
-        public static IQueryable<T> Sort<T>(this IQueryable<T> query, string sortBy, Dictionary<string, Expression<Func<T, object>>> columnsMap, bool IsSortAscending = true)
+        public static IQueryable<T> Sort<T>(this IQueryable<T> query, string sortBy, Dictionary<string, Expression<Func<T, object>>> columnsMap, bool isSortAscending = true)
         {
             if (query == null)
                 throw new ArgumentNullException("source");
 
-            if (String.IsNullOrWhiteSpace(sortBy) || !columnsMap.ContainsKey(sortBy))
+            if (string.IsNullOrWhiteSpace(sortBy) || !columnsMap.ContainsKey(sortBy))
                 return query;
 
-            if (IsSortAscending)
-                query = query.OrderBy(columnsMap[sortBy]);
-            else
-                query = query.OrderByDescending(columnsMap[sortBy]);
+            query = isSortAscending ? query.OrderBy(columnsMap[sortBy]) : query.OrderByDescending(columnsMap[sortBy]);
 
             return query;
         }
